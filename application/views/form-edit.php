20e4e80675949e7b41b54191797fef0fcbdd48b0
salsakhairinaa <salsakhairina280@gmail.com>
2018-04-12 17:30:12 +0700

    Update form-edit.php

--- application/views/form-edit.php
+++ application/views/form-edit.php
@@ -67,12 +67,102 @@
 	#container{
 		border: #D0D0D0;
 	}
+	@import url(http://fonts.googleapis.com/css?family=Roboto);
+/*--------------------------------Button starts--------------------------------*/
+.button-container {
+  text-align: center;
+}
+.button {
+  position: relative;
+  background: currentColor;
+  border: 1px solid currentColor;
+  font-size: 1.1rem;
+  color:#5e586b;
+  margin: 3rem 0;
+  padding: 0.75rem 3rem;
+  cursor: pointer;
+  -webkit-transition: background-color 0.28s ease, color 0.28s ease, box-shadow 0.28s ease;
+  transition: background-color 0.28s ease, color 0.28s ease, box-shadow 0.28s ease;
+  overflow: hidden;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+}
+.button span {
+  color: #fff;
+  position: relative;
+  z-index: 1;
+}
+.button::before {
+  content: '';
+  position: absolute;
+  background: #071017;
+  border: 50vh solid #1d4567;
+  width: 30vh;
+  height: 30vh;
+  border-radius: 50%;
+  display: block;
+  top: 50%;
+  left: 50%;
+  z-index: 0;
+  opacity: 1;
+  -webkit-transform: translate(-50%, -50%) scale(0);
+          transform: translate(-50%, -50%) scale(0);
+}
+.button:hover {
+  color: #D81900;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.2);
+}
+.button:active::before, .button:focus::before {
+  -webkit-transition: opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+  transition: opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+  transition: transform 1.12s ease, opacity 0.28s ease 0.364s;
+  transition: transform 1.12s ease, opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+  -webkit-transform: translate(-50%, -50%) scale(1);
+          transform: translate(-50%, -50%) scale(1);
+  opacity: 0;
+}
+.button:focus {
+  outline: none;
+}
+/*--------------------------------Button ends--------------------------------*/
+	* {margin:0; padding:0;}
+ 
+ nav {
+ margin:auto;
+ text-align: center;
+ width: 100%;
+ font-family: arial;
+ } 
+ nav ul {
+ background:#000f0c;
+ padding: 0 20px;
+ list-style: none;
+ position: relative;
+ display: inline-table;
+ width: 100%;
+ }
+ nav ul li{
+ float:left;
+ }
+
+ nav ul li:hover{
+ background:#d8d0d1;
+ }
+
+ nav ul li:hover a{
+ color:#000;
+ }
+
+ nav ul li a{
+ display: block;
+ padding: 25px;
+ color: #fff;
+ text-decoration: none;
 	</style>
 </head>
 <body>
 
-	<h1><b><center>Edit Your Data Here!!</center></b></h1>
-	<a class="sundaboy" href="<?php echo site_url('welcome/index')?>"><div style="background:black"><b>Back to page  </b></div></a><br>
+	<h1><b><center>Edit Your Data Here!!</center></b></h1><br>
+
 	
 
 	<?php if($dataEdit){
@@ -89,11 +179,16 @@
 	<br>
 		<font size="4"><b>Nama</b></font> <br>
 		<input type="text" name="nama" size="40" value="<?php echo $nama?>"/><br><br>
-		<font size="4"><b>Angkatan</b></font> <br>
+		<font size="4"><b>Tahun Masuk</b></font> <br>
 		<input type="text" name="angkatan" size="40" value="<?php echo $angkatan?>"/><br><br>
 		<font size="4"><b>Alamat</b></font> <br>
 		<input type="text" size="40" name="alamat" value="<?php echo $alamat?>"/><br><br>
-		<input type="submit" name="save" value="Save"/></center>
+		<div class="container">
+    <div class="button-container">
+    <button type="save" class="button"><span>Save</span></button>
+       <button class="button"  href="<?php echo site_url('welcome/index')?>"><span>Back</span></button>
+  </div> 
+</div>
 		</form><br>
 	<a href="<?php echo site_url('welcome/index')?>">Back to page</a>
 
